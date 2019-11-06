b73e93e52876c2c7cedf014fe790dcce33d6b571
Israel Oliveira <raloliver.com@gmail.com>
2019-11-05 23:17:38 -0300

    Init with smoke tests and method to verify them

--- src/main.spec.js
+++ src/main.spec.js
@@ -1,5 +1,35 @@
 import { expect } from 'chai';
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from './main';
 
-describe('Main', () => {
+describe('API Wrapper', () => {
 
+  describe('smoke tests', () => {
+
+    //search: get generics and more than one type
+    //searchAlbums
+    //searchArtists
+    //searchTracks
+    //searchPlaylists
+
+    it('should exist the `search` method', () => {
+      expect(search).to.exist;
+    });
+
+    it('should exist the `searchAlbums` method', () => {
+      expect(searchAlbums).to.exist;
+    });
+
+    it('should exist the `searchArtists` method', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('should exist the `searchTracks` method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('should exist the `searchPlaylists` method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+
+  });
 });
