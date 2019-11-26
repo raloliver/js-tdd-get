2cd3df36d2e58284a6d3fcbff8ff1f4586710dfe
Israel Oliveira <raloliver.com@gmail.com>
2019-11-26 05:00:42 -0300

    Init getAlbum() tests

---
+++ src/album.js
@@ -0,0 +1,4 @@
+export const getAlbum = () => {
+  fetch(`https://api.spotify.com/v1/albums/?ids=41MnTivkwTO3UUJ8DrqEJJ,6JWc4iAiJ9FjyK0B59ABb4,6UXCm6bOO4gFlDQZV5yL37`);
+};
+export const getAlbumTracks = () => { };
--- src/album.spec.js
+++ src/album.spec.js
@@ -1,10 +1,42 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+import { getAlbum, getAlbumTracks } from '../src/album';
+
+chai.use(sinonChai);
+
+global.fetch = require('node-fetch');
 
 describe('Album', () => {
+  let fetchStub;
+
+  beforeEach(() => {
+    fetchStub = sinon.stub(global, 'fetch');
+  });
+
+  afterEach(() => {
+    //return test to original state
+    fetchStub.restore();
+  });
 
   describe('smoke tests', () => {
     it('should have getAlbum method', () => {
       expect(getAlbum).to.exist;
     });
+    it('should have getAlbumTracks method', () => {
+      expect(getAlbumTracks).to.exist;
+    });
+  });
+
+  describe('getAlbum', () => {
+    //check fetch
+    it('should call fetch method', () => {
+      const album = getAlbum();
+      expect(fetchStub).to.have.been.calledOnce;
+    });
+    //check fetch on url
+
+    //check fetch with promise
   });
 });
