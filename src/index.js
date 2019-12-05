1034568e3f44ca369660e7417daa324cdb17e862
Israel Oliveira <raloliver.com@gmail.com>
2019-12-05 13:11:56 -0300

    Rename files and export correctly

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { search, searchAlbums, searchArtists, searchPlaylists, searchTracks } from './search';
+import { getAlbum, getAlbums, getTracks } from './album';
+
+module.exports = {
+  search, searchAlbums, searchArtists, searchPlaylists, searchTracks, getAlbum, getAlbums, getTracks
+}
