1b99d3890b81c12b17b1bcfe1dfa65a92ebb2f1b
Israel Oliveira <raloliver.com@gmail.com>
2019-12-05 13:04:40 -0300

    Resolve errors

--- src/album.js
+++ src/album.js
@@ -1,5 +1,5 @@
 import { APIURL } from './config';
 
 export const getAlbum = id => fetch(`${APIURL}/albums/${id}`).then(res => res.json());
-export const getAlbums = id => fetch(`${APIURL}/albums/?ids=${ids}`).then(data => data.json());
-export const getAlbumTracks = id => fetch(`${APIURL}/albums/${id}/tracks`).then(data => data.json());
+export const getAlbums = ids => fetch(`${APIURL}/albums/?ids=${ids}`).then(data => data.json());
+export const getTracks = id => fetch(`${APIURL}/albums/${id}/tracks`).then(data => data.json());
--- src/album.spec.js
+++ src/album.spec.js
@@ -2,7 +2,7 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 
-import { getAlbum, getAlbums, getAlbumTracks } from '../src/album';
+import { getAlbum, getAlbums, getTracks } from '../src/album';
 
 chai.use(sinonChai);
 
@@ -29,45 +29,45 @@ describe('Album', () => {
     it('should have getAlbums method', () => {
       expect(getAlbums).to.exist;
     });
-    it('should have getAlbumTracks method', () => {
-      expect(getAlbumTracks).to.exist;
+    it('should have getTracks method', () => {
+      expect(getTracks).to.exist;
     });
   });
 
   describe('getAlbum', () => {
     //check fetch
     it('should call fetch method', () => {
-      const album = getAlbum('41MnTivkwTO3UUJ8DrqEJJ');
+      const album = getAlbum();
       expect(fetchStub).to.have.been.calledOnce;
     });
     //check fetch on url
     it('should call fetch url', () => {
       const album = getAlbum('41MnTivkwTO3UUJ8DrqEJJ');
-      expect(fetchStub).to.have.been.calledWith('https://api.spotify.com/v1/albums/?ids=41MnTivkwTO3UUJ8DrqEJJ');
+      expect(fetchStub).to.have.been.calledWith('https://api.spotify.com/v1/albums/41MnTivkwTO3UUJ8DrqEJJ');
     });
     //check fetch with promise
     it('should return data', () => {
       const album = getAlbum('41MnTivkwTO3UUJ8DrqEJJ');
-      expect(album.resolveValue).to.be.eql({ album: 'name' });
+      expect(album.resolveValue).to.be.eql({ json: 'Dirty Loops' });
     });
   });
 
   describe('getAlbums', () => {
     it('should call fetch method', () => {
-      const albums = getAlbums(['4aawyAB9vmqN3uQ7FjRGTy', '4aawyAB9vmqN3uQ7FjRGTk']);
-      expect(stubedFetch).to.have.been.calledOnce;
+      const albums = getAlbums();
+      expect(fetchStub).to.have.been.calledOnce;
     });
 
     it('should call fetch with the correct URL', () => {
       const albums = getAlbums(['4aawyAB9vmqN3uQ7FjRGTy', '4aawyAB9vmqN3uQ7FjRGTk']);
-      expect(stubedFetch).to.have.been
-        .calledWith('https://api.spotify.com/v1/albums?ids=4aawyAB9vmqN3uQ7FjRGTy,4aawyAB9vmqN3uQ7FjRGTk');
+      expect(fetchStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/albums/?ids=4aawyAB9vmqN3uQ7FjRGTy,4aawyAB9vmqN3uQ7FjRGTk');
     });
 
     it('should return the correct data from Promise', () => {
       const albums = getAlbums(['4aawyAB9vmqN3uQ7FjRGTy', '4aawyAB9vmqN3uQ7FjRGTk']);
       albums.then((data) => {
-        expect(data).to.be.eql({ album: 'name' });
+        expect(data).to.be.eql({ json: 'Dirty Loops' });
       });
     });
   });
@@ -75,18 +75,18 @@ describe('Album', () => {
   describe('getAlbumsTracks', () => {
     it('should call fetch method', () => {
       const tracks = getTracks();
-      expect(stubedFetch).to.have.been.calledOnce;
+      expect(fetchStub).to.have.been.calledOnce;
     });
 
     it('should call fetch with the correct URL', () => {
       const tracks = getTracks('4aawyAB9vmqN3uQ7FjRGTy');
-      expect(stubedFetch).to.have.been
+      expect(fetchStub).to.have.been
         .calledWith('https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy/tracks');
     });
 
     it('should return the correct data from Promise', () => {
       const tracks = getTracks('4aawyAB9vmqN3uQ7FjRGTy');
-      expect(tracks.resolveValue).to.be.eql({ album: 'name' });
+      expect(tracks.resolveValue).to.be.eql({ json: 'Dirty Loops' });
     });
   });
 });
